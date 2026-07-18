a0486aede55659ec82cce48afd34905a2354d89f
Anton <a.tolkachev.dev@gmail.com>
2026-07-18 16:42:16 +0300

    feat: Add routing and font styles to the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { Routes, Route, Link } from 'react-router';
 import './App.css';
 
 function App() {
   return (
     <>
-      <div>sss</div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/about" element={<div>About</div>} />
+      </Routes>
     </>
   );
 }
